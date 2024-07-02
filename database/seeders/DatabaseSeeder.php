02107813d43567962805aae4b326613b989d5a85
Luis Martinez <luis@square1.io>
2024-07-02 13:51:35 -0500

    First Commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Order;
+use App\Models\OrderItem;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+        Category::factory(10)->create();
+
+        Product::factory(50)->create();
+
+        Order::factory(20)->create()->each(function ($order) {
+            OrderItem::factory(5)->create(['order_id' => $order->id]);
+        });
+    }
+}
